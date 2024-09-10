88fea65f7fee6487cd06f139a402d02d97b2134b
fito422480 <adolfo.andres.ayala@gmail.com>
2024-09-10 17:08:26 -0400

    Sacamos titulos

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -3,13 +3,9 @@ import BackButton from "@/components/BackButton";
 import PostsPagination from "@/components/posts/PostsPagination";
 
 const PostsPage = () => {
-  // Valores de ejemplo para limit y title
-  const title = "Todas las Facturas";
-
   return (
     <>
       <BackButton text="Atrás" link="/" />
-      <h1>{title}</h1> {/* Puedes mostrar el título aquí */}
       <PostsTable />
       <PostsPagination />
     </>
